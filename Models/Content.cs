ba454bc1f40e6f209368c5fd1261039e6aef15c9
Aayushi5899 <W0787383@myscc.ca>
2021-10-11 18:58:55 -0400

    updating

---
+++ Models/Content.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssignmentSample.Models
+{
+    public class Content
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Body { get; set; }
+        public string Tag { get; set; }
+        public string ImgUrl { get; set; }
+    }
+
+}
